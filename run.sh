e9d4d453c211f6c6346c315d615ca9f3c24a5a1d
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-03-25 01:52:15 +0800

    Add target/classes to classpath

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp target/test-trust-manager-0.0.1-SNAPSHOT-jar-with-dependencies.jar testjava.TestTrustManager
+java -cp target/classes:target/test-trust-manager-0.0.1-SNAPSHOT-jar-with-dependencies.jar testjava.TestTrustManager
